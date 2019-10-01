6918d238ef7a652ba88f278be36427dcbf97446c
Ricardo Santos <ricardo.santos@itcenter.com.pt>
2019-10-01 11:17:14 +0100

    Initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,30 @@
+const request = require("request");
+
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    "https://api.darksky.net/forecast/66399bba502f8d3ee5d4d5e3143ab7a6/" +
+    encodeURI(latitude) +
+    "," +
+    encodeURI(longitude) +
+    "?units=si&lang=pt";
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to site", undefined);
+    } else if (body.error) {
+      callback("unable to find location", undefined);
+    } else {
+      callback(
+        undefined,
+        body.daily.data[0].summary +
+          " Atualmente estão " +
+          body.currently.temperature +
+          " graus. Existe " +
+          body.currently.precipProbability +
+          "% de probabilidade de precipitação para o dia de hoje."
+      );
+    }
+  });
+};
+
+module.exports = forecast;
